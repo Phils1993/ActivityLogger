9e3fdc76a8b2b7bdd9e8b8911ceedf65155e6236
Phils1993 <phils1993@outlook.com>
2025-09-09 10:33:09 +0200

    fir commit

---
+++ src/main/java/dtos/ActivityDTO.java
@@ -0,0 +1,10 @@
+package dtos;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class ActivityDTO {
+    private LocalDate executionDate;
+    private ExerciseType exerciseType;
+
+}
---
+++ src/main/java/enums/ExerciseType.java
@@ -0,0 +1,9 @@
+package enums;
+
+public enum ExerciseType {
+    RUN,
+    SWIM,
+    BIKE,
+    HIKE,
+    WALKING;
+}
